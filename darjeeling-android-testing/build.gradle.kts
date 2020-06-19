9c4aef7fa76f9f4e6a0d3e9253fa301ba11f1791
Jaran Flaath <jaran.flaath@gmail.com>
2020-06-19 13:00:06 +0200

    Fix dependency issue. Bump version.

--- darjeeling-android-testing/build.gradle.kts
+++ darjeeling-android-testing/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = gitInfo.count
-        versionName = "0.43"
+        versionName = "0.44"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -47,7 +47,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.0")
     implementation("androidx.appcompat:appcompat:1.1.0")
 
-    debugImplementation("androidx.fragment:fragment-testing:1.2.5")
+    implementation("androidx.fragment:fragment-testing:1.2.5")
     implementation("junit:junit:4.12")
     implementation("androidx.test.ext:junit:1.1.1")
     implementation("androidx.test.espresso:espresso-core:3.2.0")
