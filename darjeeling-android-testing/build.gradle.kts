8a2872b269f8d5b1fc24df04e747db74d7910190
Jaran Flaath <jaran.flaath@gmail.com>
2020-06-22 11:31:58 +0200

    Update publishing configuration

--- darjeeling-android-testing/build.gradle.kts
+++ darjeeling-android-testing/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = gitInfo.count
-        versionName = "0.51"
+        versionName = "0.52"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -29,6 +29,24 @@ android {
 
     lintOptions {
         isAbortOnError = false
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+                // Applies the component for the release build variant.
+                from(components.getByName("release"))
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = "no.agens.darjeeling"
+                artifactId = "darjeeling-android-testing"
+                version = project.version.toString()
+                artifact("$buildDir/outputs/aar/darjeeling-android-testing-release.aar")
+            }
+        }
     }
 }
 
