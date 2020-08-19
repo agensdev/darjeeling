38d26a7e3427889db7b6449b1c064ba19ebbb0e6
Jaran Flaath <jaran.flaath@gmail.com>
2020-08-19 16:38:01 +0200

    Fix activity compatibility issues

--- darjeeling-android-testing/build.gradle.kts
+++ darjeeling-android-testing/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = gitInfo.count
-        versionName = "0.71"
+        versionName = "0.72"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
--- darjeeling-android-testing/src/main/java/no/agens/darjeeling/android/DarjeelingFragmentTest.kt
+++ darjeeling-android-testing/src/main/java/no/agens/darjeeling/android/DarjeelingFragmentTest.kt
@@ -1,6 +1,6 @@
 package no.agens.darjeeling.android
 
-import android.app.Activity
+import androidx.activity.ComponentActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.testing.FragmentScenario
@@ -11,7 +11,7 @@ abstract class DarjeelingFragmentTest<F : Fragment> : DarjeelingBase() {
     private lateinit var underlyingActivity: FragmentActivity
     lateinit var scenario: FragmentScenario<F>
 
-    override val activity: Activity
+    override val activity: ComponentActivity
         get() = underlyingActivity
 
     @Before
