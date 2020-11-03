61b88f791d9e51919a63f08dbc8f1ebe0813c1bb
Jaran Flaath <jaran.flaath@gmail.com>
2020-11-03 13:09:04 +0100

    Bump version number

--- darjeeling-android-testing/build.gradle.kts
+++ darjeeling-android-testing/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdkVersion(21)
         targetSdkVersion(29)
         versionCode = gitInfo.count
-        versionName = "0.82"
+        versionName = "0.83"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
